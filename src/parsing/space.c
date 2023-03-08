51c69b5075ecf20dc5082b887df3d34cfc2ba312
Lucas GIRAULT <lgirault@e1r10p16.clusters.42paris.fr>
2023-03-08 16:38:27 +0100

    tesst

--- src/parsing/space.c
+++ src/parsing/space.c
@@ -6,7 +6,7 @@
 /*   By: lgirault <lgirault@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/07 13:49:36 by abourdon          #+#    #+#             */
-/*   Updated: 2023/03/08 15:40:05 by lgirault         ###   ########.fr       */
+/*   Updated: 2023/03/08 16:37:18 by lgirault         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,6 +40,22 @@ int	countlen(char *str)
 		if (str[i] == '\0')
 			break ;
 		count++;
+		i++;
+	}
+	return (count);
+}
+
+int	nb_cote(char *str)
+{
+	int	i;
+	int	count;
+
+	i = 0;
+	count = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] == '\'' || str[i] == '\"')
+			count++;
 		i++;
 	}
 	return (count);
